ed102012e8c37dfd1ece205ebede3a3af4a7208e
ian-mckellar <ian.douglas.mckellar@gmail.com>
2018-03-29 15:44:23 -0600

    Cleaned up code

--- app/src/main/java/com/example/android/gameofthronesquiz/MainActivity.java
+++ app/src/main/java/com/example/android/gameofthronesquiz/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.gameofthronesquiz;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton question3Answer;
     private RadioButton question4Answer;
     private Button finishButton;
+    private EditText favoriteChar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +30,16 @@ public class MainActivity extends AppCompatActivity {
         question3Answer = (RadioButton) findViewById(R.id.q_3_answer_b);
         question4Answer = (RadioButton) findViewById(R.id.q_4_answer_c);
         finishButton = (Button) findViewById(R.id.button_finish);
-
+        favoriteChar = (EditText) findViewById(R.id.favorite_character_name);
     }
 
     public void submit(View view) {
         // right answer
+        String favoriteCharString = favoriteChar.getText().toString();
+
+        if (!favoriteCharString.isEmpty()) {
+            score +=1;
+        }
 
         if (q2AnswerB.isChecked() && q2AnswerD.isChecked()) {
             score += 1;
@@ -51,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayScore(int score) {
-        scoreTextView.setText("Score: " + score);
+        Resources resource = getResources();
+        String scoreText = resource.getString(R.string.score, score);
+        scoreTextView.setText(scoreText);
     }
 }
